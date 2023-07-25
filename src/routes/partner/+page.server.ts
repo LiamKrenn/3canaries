7178df130eea2aad278eb8a6d86b0742444fc133
LiamKrenn <k.liam1224@gmail.com>
2023-07-25 02:41:50 +0200

    server side data fetching

---
+++ src/routes/partner/+page.server.ts
@@ -0,0 +1,21 @@
+import type { Partner } from '$lib/Types';
+import type { PageServerLoad } from './$types';
+import PocketBase, { Record } from 'pocketbase';
+
+export const load = (async () => {
+	const pb: PocketBase = new PocketBase('http://127.0.0.1:8090');
+	const result: Partner[] = await pb.collection('partner').getFullList<Partner>();
+    const p: Partner[] = structuredClone(result)
+
+
+
+    console.log(pb.getFileUrl(p[0], p[0].logo));
+
+    for (const obj of p) {
+        obj.logourl = pb.getFileUrl(obj, obj.logo)
+    }
+    
+	return {
+		partners: p
+	};
+}) satisfies PageServerLoad;
