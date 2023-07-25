a77968fbd0d0af961622e207976a465ac97c5434
LiamKrenn <k.liam1224@gmail.com>
2023-07-25 02:40:48 +0200

    added Partner DB type

---
+++ src/lib/Types.ts
@@ -0,0 +1,12 @@
+export interface Partner {
+	collectionId: string;
+	collectionName: string;
+	created: string;
+	description: string;
+	id: string;
+	link: string;
+	logo: string;
+  logourl: string;
+	name: string;
+	updated: string;
+}
