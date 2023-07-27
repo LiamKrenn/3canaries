9bcacfc17422aafba3bf75553de4ae06085baab6
LiamKrenn <k.liam1224@gmail.com>
2023-07-28 00:12:29 +0200

    Added Carousel, Times in format

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,36 @@
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
 <script lang="ts">
+	import Carousel from "$lib/components/Carousel.svelte";
+	import type { PageData } from './$types';
+	export let data: PageData;
 
+	function shortDate(date: string) {
+		let d = new Date(date)
+		const options: Intl.DateTimeFormatOptions = {
+				weekday: 'short',
+				day: 'numeric',
+				month: 'long'
+		};
+		const formattedDate = d.toLocaleDateString('de-DE', options);
+		console.log(formattedDate);
+		return formattedDate
+	}
+	
 </script>
 
-<div class="container h-full mx-auto flex justify-center items-center">
+
 	
-	<div class="space-y-5">
-		
-		<h1 class="h1">Vinyl</h1>
-	</div>
-	a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>
-	a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>
+<Carousel/>
+
+<div class="w-full flex justify-center mt-16">
+	<h1 class="h1 w-fit">Aktuelle Öffnungszeiten</h1>
+	
 </div>
+<div class="flex w-screen h-fit flex-col items-center mt-4">
+	{#each data.times as time}
+	<p class="h-fit">{shortDate(time.from)}</p>
+	
+{/each}
+</div>
+
