c58dc7e2eb2b45e0a2fdf13bb5fa5bdcf1016800
LiamKrenn <k.liam1224@gmail.com>
2023-07-28 00:11:43 +0200

    added logo

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -3,9 +3,14 @@
 
 
 </script>
-
-<div class="p-2 mb-2 flex justify-center">
+<div class="w-full flex justify-center p-2  mt-8 ">
+	<img class="w-80 " src="images/C_TC_text_cut.webp"/>
+</div>
+<div class="w-full flex justify-center px-2 pb-4">
+	<img class="w-60 " src="images/R_text_cut.webp"/>
+</div>
+<div class="p-2 mb-2 flex flex-row flex-wrap justify-center">
   {#each footerItems as item}
-		<a class="px-2  underline" href={item.link}>{item.title}</a>
+		<a class="px-2  underline whitespace-nowrap" href={item.link}>{item.title}</a>
 	{/each}
 </div>
