830a48b7d58fad51452dd622b9dd043b8c37b2c0
LiamKrenn <k.liam1224@gmail.com>
2023-07-30 22:20:54 +0200

    homepage content progress

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,72 @@
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
 <script lang="ts">
-	import Carousel from "$lib/components/Carousel.svelte";
+	import Carousel from '$lib/components/Carousel.svelte';
+	import ChevronRight from '$lib/icons/chevron-right.svelte';
 	import type { PageData } from './$types';
 	export let data: PageData;
 
 	function shortDate(date: string) {
-		let d = new Date(date)
+		let d = new Date(date);
 		const options: Intl.DateTimeFormatOptions = {
-				weekday: 'short',
-				day: 'numeric',
-				month: 'long'
+			weekday: 'short',
+			day: 'numeric',
+			month: 'long'
 		};
 		const formattedDate = d.toLocaleDateString('de-DE', options);
 		console.log(formattedDate);
-		return formattedDate
+		return formattedDate;
 	}
-	
 </script>
 
 
-	
-<Carousel/>
+<Carousel />
 
-<div class="w-full flex justify-center mt-16">
-	<h1 class="h1 w-fit">Aktuelle Öffnungszeiten</h1>
-	
+
+<div
+	class="w-full flex flex-col justify-center text-center items-center pt-6 pb-4 bg-primary-400 select-none"
+>
+	<h1 class="h2 w-fit dark:text-black">Aktuelle Öffnungszeiten</h1>
+	<div class="flex flex-col items-start ">
+		{#each data.times as time}
+			<p class=" dark:text-black text-2xl mt-4 whitespace-nowrap">{shortDate(time.from)}</p>
+			<p class="text-2xl dark:text-black font-thin">{time.event}</p>
+		{/each}
+	</div>
+	<a
+		href="/open-times"
+		class="text-xl pb-4 px-4 pt-4 mt-2 underline flex items-center cursor-pointer dark:text-black"
+		>Alle Öffnungszeiten<ChevronRight /></a
+	>
 </div>
-<div class="flex w-screen h-fit flex-col items-center mt-4">
-	{#each data.times as time}
-	<p class="h-fit">{shortDate(time.from)}</p>
-	
-{/each}
+<div class="w-full pt-8 pb-6 justify-center text-center">
+	<h1 class="h1 font-bold ">more than a vinyl shop.</h1>
 </div>
 
+
+<div class="w-full flex flex-row flex-wrap py-2 justify-center items-start ">
+	<div class="card w-[40vw] h-max-[30rem] mx-4 my-2 rounded-lg variant-filled-secondary">
+		<header>
+			<img class=" w-[40vw]  rounded-lg shrink-0 " src="images/meinladen.webp" />
+		</header>
+		<div class="p-6">
+			<h3 class="h3 mb-2">Mein Laden.</h3>
+			<p class="whitespace-break-spaces">
+				Mein Plattenladen in Graz (Steiermark) ist nicht nur "Geschäft" - es ist Lebensgefühl. Ein Ort, bei dem Zeit keine Rolle spielt. Ein Ort der Leidenschaft & Hingabe zur Kunst Musik. "More than an Vinyl Shop" eben. Aus purer Überzeugung...
+			</p>
+		</div>
+	</div>
+
+
+	<div class="card w-[40vw] mx-4 my-2 rounded-lg variant-filled-secondary">
+		<header>
+			<img class=" w-[40vw]  rounded-lg shrink-0" src="images/meinplatten.webp" />
+		</header>
+		<div class="p-6">
+			<h3 class="h3 mb-2">Meine Platten.</h3>
+			<p class="whitespace-break-spaces">
+				Im Laufe der Zeit sammelten sich die Werke. Ohne Druck, ohne Stress. Die nunmehr exklusive Vinyl-Kollektion beinhaltet seltenste Ausgaben und echte Schmuckstücke. Wenn es nach meinem Geschmack geht, versteht sich. Vielleicht findest du hier genau deine Platte...
+			</p>
+		</div>
+	</div>
+</div>
