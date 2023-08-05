527abf70b5dbd21c42f51d70614bdb9b82c620cf
LiamKrenn <k.liam1224@gmail.com>
2023-08-06 00:02:26 +0200

    only show first 3 results on main page

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -5,7 +5,7 @@ import PocketBase, { ListResult } from 'pocketbase';
 export const load = (async () => {
 	const pb: PocketBase = new PocketBase('http://127.0.0.1:8090');
 	const date = new Date().toJSON().slice(0, 10);
-	const result: ListResult<Times> = await pb.collection('times').getList<Times>(1, 100, {
+	const result: ListResult<Times> = await pb.collection('times').getList<Times>(1, 3, {
 		filter: `from >= "${date} 00:00:00"`
 	});
 	const p: Times[] = structuredClone(result.items);
