fc24c081a5abcc99ccd6465c00ef7411b60633d7
LiamKrenn <k.liam1224@gmail.com>
2023-08-06 00:02:00 +0200

    time card

---
+++ src/lib/components/OpenTimeCard.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { Times } from '$lib/Types';
+
+	export let time: Times;
+
+  function shortDate(date: string) {
+		let d = new Date(date);
+		const options: Intl.DateTimeFormatOptions = {
+			weekday: 'short',
+			day: 'numeric',
+			month: 'long'
+		};
+		const formattedDate = d.toLocaleDateString('de-DE', options);
+		console.log(formattedDate);
+		return formattedDate;
+	}
+</script>
+
+<div class="card w-28 mx-1 p-2"><p>{shortDate(time.from)}</p></div>
