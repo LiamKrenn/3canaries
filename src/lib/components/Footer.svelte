e1b19e468839be5a2dcdc5b7ec28b4f236a7013e
LiamKrenn <k.liam1224@gmail.com>
2023-08-06 20:33:26 +0200

    image no select

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -3,10 +3,10 @@
 
 
 </script>
-<div class="w-full flex justify-center p-2  mt-8 ">
+<div class="w-full flex justify-center p-2  mt-8 select-none">
 	<img class="w-80 " src="images/C_TC_text_cut.webp"/>
 </div>
-<div class="w-full flex justify-center px-2 pb-4">
+<div class="w-full flex justify-center px-2 pb-4 select-none">
 	<img class="w-60 " src="images/R_text_cut.webp"/>
 </div>
 <div class="p-2 mb-2 flex flex-row flex-wrap justify-center">
