5a5e01765cfa2ffa585520101f2d26f7ca4922b0
LiamKrenn <k.liam1224@gmail.com>
2023-08-06 20:33:44 +0200

    sort from early to late

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -6,7 +6,8 @@ export const load = (async () => {
 	const pb: PocketBase = new PocketBase('http://127.0.0.1:8090');
 	const date = new Date().toJSON().slice(0, 10);
 	const result: ListResult<Times> = await pb.collection('times').getList<Times>(1, 3, {
-		filter: `from >= "${date} 00:00:00"`
+		filter: `from >= "${date} 00:00:00"`,
+		sort: 'from'
 	});
 	const p: Times[] = structuredClone(result.items);
 
