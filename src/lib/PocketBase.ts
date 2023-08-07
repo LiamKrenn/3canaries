fd8766f9ada8d4f8ed9ac6368983c8a0850902b0
LiamKrenn <k.liam1224@gmail.com>
2023-08-07 23:28:13 +0200

    moved url to variable, added getNews

--- src/lib/PocketBase.ts
+++ src/lib/PocketBase.ts
@@ -1,15 +1,21 @@
 import PocketBase, { ListResult } from 'pocketbase';
-import type { Times } from '$lib/Types';
+import type { News, Times } from '$lib/Types';
 
-const pb: PocketBase = new PocketBase('http://127.0.0.1:8090');
+const domain: string = '127.0.0.1:8090';
+export const url: string = `http://${domain}/api/files`;
+const pb: PocketBase = new PocketBase(`http://${domain}`);
 
 export async function getTimesPerMonth(d: Date): Promise<Times[]> {
 	const monthEnd = new Date(d.getFullYear(), d.getMonth() + 1, 1).toJSON().slice(0, 10);
 	const monthStart = new Date(d.getFullYear(), d.getMonth(), 2).toJSON().slice(0, 10);
-  
 	const result: ListResult<Times> = await pb.collection('times').getList<Times>(1, 999999, {
 		filter: `from >= "${monthStart} 00:00:00" && from <= "${monthEnd} 00:00:00"`,
 		sort: 'from'
 	});
 	return structuredClone(result.items);
+}
+
+export async function getNews(): Promise<News[]> {
+	const result: News[] = await pb.collection('news').getFullList<News>({ sort: '-display_date' });
+	return structuredClone(result);
 }
