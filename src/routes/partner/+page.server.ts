6ce272e75094e027c685616c48d535e36e2260ee
LiamKrenn <k.liam1224@gmail.com>
2023-08-07 23:27:14 +0200

    removed function call for image url

--- src/routes/partner/+page.server.ts
+++ src/routes/partner/+page.server.ts
@@ -5,12 +5,8 @@ import PocketBase, { Record } from 'pocketbase';
 export const load = (async () => {
 	const pb: PocketBase = new PocketBase('http://127.0.0.1:8090');
 	const result: Partner[] = await pb.collection('partner').getFullList<Partner>();
-    const p: Partner[] = structuredClone(result)
+	const p: Partner[] = structuredClone(result);
 
-    for (const obj of p) {
-        obj.logourl = pb.getFileUrl(obj, obj.logo)
-    }
-    
 	return {
 		partners: p
 	};
