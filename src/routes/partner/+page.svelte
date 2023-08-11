c6e9328f3d14837d858c1ac3dc78bd14aa9faa97
LiamKrenn <k.liam1224@gmail.com>
2023-08-11 22:39:26 +0200

    fixed flex wrap

--- src/routes/partner/+page.svelte
+++ src/routes/partner/+page.svelte
@@ -11,9 +11,9 @@
 	class="py-20 w-full flex flex-col justify-center items-center bg-secondary-500 dark:bg-secondary-600 dark:text-surface-900"
 >
 	<h1 class="h1">Unsere Partner...</h1>
-	<div class="flex mt-16 flex-row flex-wrap h-full">
+	<div class="flex mt-16 flex-row flex-wrap h-full justify-center">
 		{#each data.partners as partner}
-			<div class="mx-4 shrink-0 h-full">
+			<div class="mx-4 shrink-0 h-full mb-8">
 				<img class="h-[330px]" src={`${url}/${partner.collectionId}/${partner.id}/${partner.logo}`} alt={`${partner.name} Logo`} />
 				<h1 class="text-3xl mt-6 h1">{@html partner.name}</h1>
 				<p class="text-xl mt-4 w-[330px]">{partner.description}</p>
