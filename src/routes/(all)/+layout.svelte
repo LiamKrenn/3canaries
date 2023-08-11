3cac2824fd8a55030da42271e646705efeba67b5
LiamKrenn <k.liam1224@gmail.com>
2023-08-12 00:06:08 +0200

    routes in group: error page not affected by layout

---
+++ src/routes/(all)/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { AppBar, AppShell, Drawer, LightSwitch } from '@skeletonlabs/skeleton';
+	import SideNav from '$lib/components/SideNav.svelte';
+	import NavItem from '$lib/components/NavItem.svelte';
+	import NavBar from '$lib/components/NavBar.svelte';
+	import { items } from '$lib/NavItems';
+	import Footer from '$lib/components/Footer.svelte';
+	
+</script>
+
+<AppShell class="bg-white dark:bg-surface-800">
+	<svelte:fragment slot="header">
+		<AppBar padding="p-0" gap="gap-0" background="bg-white dark:bg-surface-800">
+			<NavBar />
+		</AppBar>
+	</svelte:fragment>
+	<svelte:fragment slot="pageFooter">
+		<Footer />
+	</svelte:fragment>
+	<slot />
+</AppShell>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,28 +1,9 @@
 <script lang="ts">
-	// Your custom Skeleton theme:
 	import '../theme.postcss';
 	// This contains the bulk of Skeletons required styles:
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	// Finally, your application's global stylesheet (sometimes labeled 'app.css')
 	import '../app.postcss';
-
-	import { AppBar, AppShell, Drawer, LightSwitch } from '@skeletonlabs/skeleton';
-	import SideNav from '$lib/components/SideNav.svelte';
-	import NavItem from '$lib/components/NavItem.svelte';
-	import NavBar from '$lib/components/NavBar.svelte';
-	import { items } from '$lib/NavItems';
-	import Footer from '$lib/components/Footer.svelte';
-	
 </script>
 
-<AppShell class="bg-white dark:bg-surface-800">
-	<svelte:fragment slot="header">
-		<AppBar padding="p-0" gap="gap-0" background="bg-white dark:bg-surface-800">
-			<NavBar />
-		</AppBar>
-	</svelte:fragment>
-	<svelte:fragment slot="pageFooter">
-		<Footer />
-	</svelte:fragment>
-	<slot />
-</AppShell>
+<slot />
