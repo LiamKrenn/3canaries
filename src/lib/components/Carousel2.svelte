473373025e29a3dc774b75a15285724e44fc6d4c
LiamKrenn <k.liam1224@gmail.com>
2023-08-13 22:06:11 +0200

    fixed stroke added images

--- src/lib/components/Carousel2.svelte
+++ src/lib/components/Carousel2.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
 	import ChevronLeft from '$lib/icons/chevron-left.svelte';
 	import ChevronRight from '$lib/icons/chevron-right.svelte';
-	let images = ['title1hq', 'title2hq', 'title3hq'];
+	const images = ['scnd1hq', 'scnd2hq', 'scnd3hq', 'scnd4hq'];
 
 	let elemCarousel: HTMLDivElement;
 	let counter: number = 0;
-	const pages: number = 3;
+	const pages: number = images.length;
 
 	function carouselLeft(): void {
 		counter = (counter - 1 + pages) % pages;
@@ -29,7 +29,7 @@
 <div class="grid items-center relative">
 	<div bind:this={elemCarousel} class="snap-x snap-mandatory scroll-smooth flex overflow-y-hidden overflow-x-hidden">
 		{#each images as image}
-			<div class="min-w-full h-[50vh] flex items-center justify-center my-[-7vh] relative">
+			<div class="min-w-full h-[50vh] flex items-center justify-center relative">
 				<img
 					class="min-w-full h-[50vh] object-cover "
 					src="images/{image}.webp"
@@ -41,12 +41,16 @@
 			</div>
 		{/each}
 	</div>
-	<button type="button" class="btn-icon variant-filled absolute left-[10%] top-[50%] " on:click={carouselLeft}>
+	<button
+		type="button"
+		class="btn-icon variant-filled absolute sm:scale-100 scale-75 lg:left-[10%] md:left-[3%] left-[1%] top-[50%] stroke-current"
+		on:click={carouselLeft}
+	>
 		<ChevronLeft />
 	</button>
 	<button
 		type="button"
-		class="btn-icon variant-filled absolute right-[10%]"
+		class="btn-icon variant-filled absolute sm:scale-100 scale-75 lg:right-[10%] md:right-[3%] right-[1%] top-[50%] stroke-current"
 		on:click={carouselRight}
 	>
 		<ChevronRight />
