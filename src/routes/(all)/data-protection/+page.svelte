76109ff95c26f942e2f8069063f6727499269aea
LiamKrenn <k.liam1224@gmail.com>
2023-08-14 00:29:12 +0200

    Datenschutzrichtlinien

---
+++ src/routes/(all)/data-protection/+page.svelte
@@ -0,0 +1,122 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>3Canaries | Datenschutzerklärung</title>
+	<meta name="description" content="Datenschutzerklärung von Three Canaries records." />
+</svelte:head>
+
+<div class="h-min w-full md:px-[25%] px-[10%] justify-center">
+	<h1 class="h1 my-8">Datenschutzerklärung</h1>
+	<p>
+		Wir nehmen den Schutz Ihrer persönlichen Daten sehr ernst. Wir behandeln Ihre personenbezogenen
+		Daten vertraulich und entsprechend der gesetzlichen Datenschutzvorschriften sowie dieser
+		Datenschutzerklärung.<br /><br />
+
+		Die Nutzung unserer Website ist in der Regel ohne Angabe personenbezogener Daten möglich. Soweit
+		auf unseren Seiten personenbezogene Daten (beispielsweise Name, Anschrift oder E-Mail-Adressen)
+		erhoben werden, erfolgt dies, soweit möglich, stets auf freiwilliger Basis. Diese Daten werden
+		ohne Ihre ausdrückliche Zustimmung nicht an Dritte weitergegeben.<br /><br />
+
+		Wir weisen darauf hin, dass die Datenübertragung im Internet (z.B. bei der Kommunikation per
+		E-Mail) Sicherheitslücken aufweisen kann. Ein lückenloser Schutz der Daten vor dem Zugriff durch
+		Dritte ist nicht möglich.<br /><br />
+
+		<br /><span class="font-bold">Cookies</span> <br />
+
+		Die Internetseiten verwenden teilweise so genannte Cookies. Cookies richten auf Ihrem Rechner
+		keinen Schaden an und enthalten keine Viren. Cookies dienen dazu, unser Angebot
+		nutzerfreundlicher, effektiver und sicherer zu machen. Cookies sind kleine Textdateien, die auf
+		Ihrem Rechner abgelegt werden und die Ihr Browser speichert.<br /><br />
+
+		Bitte lesen Sie unsere <a class="underline" href="/cookies">Cookie-Richtlinien</a> um mehr zu
+		erfahren.<br /><br />
+
+		<br /><span class="font-bold">Server-Log-Files</span> <br />
+
+		Der Provider der Seiten erhebt und speichert automatisch Informationen in so genannten
+		Server-Log Files, die Ihr Browser automatisch an uns übermittelt. Dies sind: <br /><br />
+
+		- Browsertyp und Browserversion<br />
+		- verwendetes Betriebssystem<br />
+		- Referrer URL<br />
+		- Hostname des zugreifenden Rechners<br />
+		- Uhrzeit der Serveranfrage<br /><br />
+
+		Diese Daten sind nicht bestimmten Personen zuordenbar. Eine Zusammenführung dieser Daten mit
+		anderen Datenquellen wird nicht vorgenommen. Wir behalten uns vor, diese Daten nachträglich zu
+		prüfen, wenn uns konkrete Anhaltspunkte für eine rechtswidrige Nutzung bekannt werden.<br /><br
+		/>
+
+		<br /><span class="font-bold">Google Maps</span> <br />
+
+		Diese Seite nutzt über eine API den Kartendienst Google Maps. Anbieter ist die Google Inc., 1600
+		Amphitheatre Parkway Mountain View, CA 94043, USA.<br /><br />
+
+		Zur Nutzung der Funktionen von Google Maps ist es notwendig, Ihre IP Adresse zu speichern. Diese
+		Informationen werden in der Regel an einen Server von Google in den USA übertragen und dort
+		gespeichert. Der Anbieter dieser Seite hat keinen Einfluss auf diese Datenübertragung.<br /><br
+		/>
+
+		Die Nutzung von Google Maps erfolgt im Interesse einer ansprechenden Darstellung unserer
+		Online-Angebote und an einer leichten Auffindbarkeit der von uns auf der Website angegebenen
+		Orte. Dies stellt ein berechtigtes Interesse im Sinne von Art. 6 Abs. 1 lit. f DSGVO dar.<br
+		/><br />
+
+		Mehr Informationen zum Umgang mit Nutzerdaten finden Sie in der Datenschutzerklärung von Google:
+		<a
+			href="https://policies.google.com/privacy"
+			target="_blank"
+			rel="noreferrer"
+			class="underline text-blue-500">Google Datenschutzerklärung</a
+		>.<br /><br />
+
+		<br /><span class="font-bold">Cloudflare</span> <br />
+
+		Unsere Website nutzt Funktionen von Cloudflare als Sicherheitsmaßnahme. Anbieter ist die
+		Cloudflare Inc., 101 Townsend St, San Francisco, CA 94107, USA.<br /><br />
+
+		Cloudflare bietet ein weltweit verteiltes Content Delivery Network mit DNS an. Technisch wird
+		der Informationstransfer zwischen Ihrem Browser und unserer Website über das Netzwerk von
+		Cloudflare geleitet. Cloudflare ist damit in der Lage, den Datenverkehr zwischen Benutzern und
+		unserer Website zu analysieren, um beispielsweise Angriffe auf unsere Dienste zu erkennen und
+		abzuwehren. Zudem speichert Cloudflare unter Umständen bestimmte Informationen, um die
+		Sicherheit und Performance des Netzwerks zu optimieren.<br /><br />
+
+		Die Nutzung von Cloudflare erfolgt im Interesse einer sicheren Bereitstellung unserer
+		Online-Angebote. Dies stellt ein berechtigtes Interesse im Sinne von Art. 6 Abs. 1 lit. f DSGVO
+		dar.<br /><br />
+
+		Weitere Informationen finden Sie in der Datenschutzerklärung von Cloudflare:
+		<a
+			href="https://www.cloudflare.com/privacypolicy/"
+			target="_blank"
+			rel="noreferrer"
+			class="underline text-blue-500">Cloudflare Datenschutzerklärung</a
+		>.<br /><br />
+
+		<br /><span class="font-bold">Vercel</span> <br />
+
+		Unsere Website nutzt Funktionen von Vercel als Content Delivery Network (CDN). Anbieter ist die
+		Vercel Inc., 340 S Lemon Ave #4133 Walnut, CA 91789, USA.<br /><br />
+
+		Vercel bietet ein weltweit verteiltes Content Delivery Network an, das es uns ermöglicht, unsere
+		Website schnell und zuverlässig bereitzustellen. Technisch wird der Informationstransfer
+		zwischen Ihrem Browser und unserer Website über das Netzwerk von Vercel geleitet. Vercel ist
+		damit in der Lage, den Datenverkehr zwischen Benutzern und unserer Website zu analysieren, um
+		beispielsweise die Performance und Sicherheit des Netzwerks zu optimieren.<br /><br />
+
+		Die Nutzung von Vercel erfolgt im Interesse einer schnellen und zuverlässigen Bereitstellung
+		unserer Online-Angebote. Dies stellt ein berechtigtes Interesse im Sinne von Art. 6 Abs. 1 lit.
+		f DSGVO dar.<br /><br />
+
+		Weitere Informationen finden Sie in der Datenschutzerklärung von Vercel:
+
+		<a
+			href="https://vercel.com/legal/privacy-policy"
+			target="_blank"
+			rel="noreferrer"
+			class="underline text-blue-500">Vercel Datenschutzerklärung</a
+		>.<br /><br />
+	</p>
+</div>
