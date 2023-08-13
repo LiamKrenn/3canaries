9ace5e7c2490131a411f0e945d6e691ff854829a
LiamKrenn <k.liam1224@gmail.com>
2023-08-14 00:29:17 +0200

    Impressum

---
+++ src/routes/(all)/imprint/+page.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>3Canaries | Impressum</title>
+	<meta name="description" content="Impressum von Three Canaries records." />
+</svelte:head>
+
+<div class="h-min w-full  md:px-[25%] px-[10%]  justify-center">
+	<h1 class="h1 my-8">Impressum</h1>
+	<p class="font-bold mb-4">Three Canaries records.</p>
+	<p>
+		Am Langedelwehr 34/1, 8010 Graz<br /><br />
+
+		contact@3canaries.com<br />
+		0677 64 82 28 33<br />
+		3canaries.com<br /><br />
+
+		<span class="italic">Für den Inhalt verantwortlich:</span><br />Stefan Krenn, Flurgasse 258,
+		8181 St. Ruprecht/Raab
+	</p>
+
+	<p class="font-bold pt-8 pb-4">Copyright / Haftungsausschluss</p>
+	<p>
+		Bild- und Datenmaterial unterliegen dem Copyright des Betreibers und dürfen ohne ausdrückliche
+		Zustimmung des Betreibers nicht verwendet werden. Die Website www.3canaries.com enthält Links zu
+		Webseiten Dritter, die von anderen unterhalten werden. Diese Links sind rein für den gefälligen
+		Gebrauch zur Verfügung gestellt und stellen keine Billigung des Inhalts auf diesen genannten
+		Seiten von Seiten des Betreibers dar. Der Betreiber ist für den Inhalt dieser Links nicht
+		verantwortlich und steht auch nicht für den Inhalt und die Genauigkeit des Materials auf den
+		Webseiten Dritter ein. Wenn Sie sich für einen Zugriff auf diese Links zu den Webseiten Dritter
+		entscheiden, tun Sie dies auf eigene Verantwortung.
+	</p>
+	<p class="font-bold italic pt-4">
+		FOTOCREDIT: <span class="font-normal not-italic">Foto Ripix</span>
+	</p>
+
+	<p class="mt-16">
+		Bildnachweis: <a
+			class="underline"
+			href="http://unsplash.com/photos/dpCgZJoneZM"
+			target="_blank"
+			rel="noreferrer"
+		>
+			Ryan Riggins</a
+		>
+		on
+		<a class="underline" href="https://unsplash.com/" target="_blank" rel="noreferrer">Unsplash</a>,
+		<a
+			class="underline"
+			href="https://unsplash.com/photos/hE7yn-pSU3s"
+			target="_blank"
+			rel="noreferrer">Tim Wright</a
+		>
+		on
+
+		<a class="underline" href="https://unsplash.com/" target="_blank" rel="noreferrer">Unsplash</a>
+	</p>
+</div>
