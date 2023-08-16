f8d18023b16d9957060fdac0e8c5c796b682c47d
LiamKrenn <k.liam1224@gmail.com>
2023-08-16 19:27:10 +0200

    fixed card message tab indent

--- src/routes/(all)/+page.svelte
+++ src/routes/(all)/+page.svelte
@@ -48,10 +48,11 @@
 	<h1 class="h2 w-fit dark:text-black">Aktuelle Öffnungszeiten</h1>
 	<div class="flex flex-col items-start">
 		{#each data.times as time}
-			<p class=" dark:text-black text-2xl mt-4 whitespace-nowrap flex items-center justify-between w-full">
+			<p
+				class=" dark:text-black text-2xl mt-4 whitespace-nowrap flex items-center justify-between w-full"
+			>
 				{shortDate(time.from)}
-				<span class="text-xl font-thin ml-2.5 text-right">{fromToText(time)}</span
-				>
+				<span class="text-xl font-thin ml-2.5 text-right">{fromToText(time)}</span>
 			</p>
 			<p class="text-2xl dark:text-black font-thin">{time.event}</p>
 		{/each}
@@ -83,9 +84,7 @@
 		<div class="md:p-6 p-4">
 			<h3 class="h3 mb-2">Mein Laden.</h3>
 			<p class="whitespace-break-spaces">
-				Mein Plattenladen in Graz (Steiermark) ist nicht nur "Geschäft" - es ist Lebensgefühl. Ein
-				Ort, bei dem Zeit keine Rolle spielt. Ein Ort der Leidenschaft & Hingabe zur Kunst Musik.
-				"More than an Vinyl Shop" eben. Aus purer Überzeugung...
+				{`Mein Plattenladen in Graz (Steiermark) ist nicht nur "Geschäft" - es ist Lebensgefühl. Ein Ort, bei dem Zeit keine Rolle spielt. Ein Ort der Leidenschaft & Hingabe zur Kunst Musik. "More than an Vinyl Shop" eben. Aus purer Überzeugung...`}
 			</p>
 		</div>
 	</div>
@@ -103,9 +102,8 @@
 		<div class="md:p-6 p-4">
 			<h3 class="h3 mb-2">Meine Platten.</h3>
 			<p class="whitespace-break-spaces">
-				Im Laufe der Zeit sammelten sich die Werke. Ohne Druck, ohne Stress. Die nunmehr exklusive
-				Vinyl-Kollektion beinhaltet seltenste Ausgaben und echte Schmuckstücke. Wenn es nach meinem
-				Geschmack geht, versteht sich. Vielleicht findest du hier genau deine Platte...
+				{`Im Laufe der Zeit sammelten sich die Werke. Ohne Druck, ohne Stress. Die nunmehr exklusive Vinyl-Kollektion beinhaltet seltenste Ausgaben und echte Schmuckstücke. Wenn es nach meinem Geschmack geht, versteht sich. Vielleicht findest du hier genau deine Platte...`}
+				
 			</p>
 		</div>
 	</div>
