782a86f5a8c92d4d08bdcfe522512e107b65f551
LiamKrenn <k.liam1224@gmail.com>
2023-08-20 01:59:33 +0200

    test revalidate of all pages

--- src/routes/revalidate/+server.ts
+++ src/routes/revalidate/+server.ts
@@ -1,15 +1,24 @@
 import { FETCH_BYPASS_KEY } from '$env/static/private';
 import type { RequestHandler } from '@sveltejs/kit';
 
+const routes: string[] = ['/', '/partner', '/open-times', 'news'];
 
+async function revalidateRoute(route: string) {
+	return await fetch('3canaries' + route, {
+		headers: {
+			'x-my-custom-header': FETCH_BYPASS_KEY
+		}
+	});
+}
 
-export const GET: RequestHandler = ({ request }) => {
-  console.log(request.headers);
-  if (request.headers.get('x-prerender-revalidate') === FETCH_BYPASS_KEY) {
-    return new Response("yippi");
-  }
-
-  return new Response("t");
+export const GET: RequestHandler = async ({ request }) => {
+	if (request.headers.get('x-prerender-revalidate') === FETCH_BYPASS_KEY) {
+		routes.forEach((route) => {
+			revalidateRoute(route);
+		});
+		return new Response('Success');
+	}
+	return new Response('Invalid');
 };
 
 //export function GET: RequestHandler = ({url}) => {
