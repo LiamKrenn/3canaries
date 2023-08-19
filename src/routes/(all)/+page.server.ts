450a2488390555596a0374a1770a529da53d67d6
LiamKrenn <k.liam1224@gmail.com>
2023-08-19 18:21:57 +0200

    test isr

--- src/routes/(all)/+page.server.ts
+++ src/routes/(all)/+page.server.ts
@@ -3,6 +3,15 @@ import type { Times } from '$lib/Types';
 import type { PageServerLoad } from './$types';
 import PocketBase, { ListResult } from 'pocketbase';
 
+import { TEST_KEY } from '$env/static/private';
+
+export const config = {
+  isr: {
+    expiration: 300,
+    bypassToken: TEST_KEY,
+  },
+};
+
 export const load = (async () => {
 	const pb: PocketBase = new PocketBase(`https://${domain}`);
 	const date = new Date().toJSON().slice(0, 10);
