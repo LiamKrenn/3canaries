dbe9092f29e8fbf5cddc8bd411597f8c54326b01
LiamKrenn <k.liam1224@gmail.com>
2023-08-19 17:43:44 +0200

    merged components into one

--- src/lib/components/Image.svelte
+++ src/lib/components/Image.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
-	import Picture from './Picture.svelte';
+
+	type ImgProps = {
+		src: string;
+		w: number;
+	};
+	type ImgPropsH = ImgProps & { h: number };
+	type ImgMeta = { img: ImgPropsH; sources: { [key: string]: ImgProps[] } };
 
 	export let mount: boolean;
 
 	export let alt: string;
 
-	export let hqimage: any;
+	export let hqimage: ImgMeta[];
 	export let hqclass: string;
 	export let hqheight: string;
 	export let hqwidth: string;
@@ -18,21 +24,35 @@
 	export let lqloading: 'lazy' | 'eager' | null | undefined = 'eager';
 
 	let loaded: boolean = false;
+
+	// HQ
+	// if there is only one, vite-imagetools won't wrap the object in an array
+	if (!(hqimage instanceof Array)) hqimage = [hqimage];
+	const sources = hqimage[0].sources;
+	const fallback = hqimage[0].img;
 </script>
 
 {#if !loaded}
 	<img class={lqclass} src={lqsrc} {alt} loading={lqloading} height={lqheight} width={lqwidth} />
 {/if}
+
 {#if mount}
-	<Picture
-		meta={hqimage}
-		loading={hqloading}
-		{alt}
-		height={hqheight}
-		width={hqwidth}
-		imageClass="{hqclass} {loaded ? '' : 'absolute top-0 left-0'}"
-		on:load={() => {
-			loaded = true;
-		}}
-	/>
+	<div style={`max-width: ${fallback.w}px`}>
+		<picture class="">
+			{#each Object.entries(sources) as [type, srcMeta]}
+				<source type="image/{type}" srcset={srcMeta.map((m) => `${m.src} ${m.w}w`).join(', ')} />
+			{/each}
+			<img
+				on:load={() => {
+					loaded = true;
+				}}
+				src={fallback.src}
+				{alt}
+				loading={hqloading}
+				class="{hqclass} {loaded ? '' : 'absolute top-0 left-0'}"
+				width={hqwidth}
+				height={hqheight}
+			/>
+		</picture>
+	</div>
 {/if}
