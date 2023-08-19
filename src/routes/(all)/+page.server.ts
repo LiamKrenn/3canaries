7e614ed5c6583159cbc5549124088d6b007a3faa
LiamKrenn <k.liam1224@gmail.com>
2023-08-20 00:24:38 +0200

    fixed timezone bug

--- src/routes/(all)/+page.server.ts
+++ src/routes/(all)/+page.server.ts
@@ -2,21 +2,22 @@ import { domain } from '$lib/PocketBase';
 import type { Times } from '$lib/Types';
 import type { PageServerLoad } from './$types';
 import PocketBase, { ListResult } from 'pocketbase';
+import moment from 'moment-timezone';
 
 import { FETCH_BYPASS_KEY } from '$env/static/private';
 
 export const config = {
-  isr: {
-    expiration: 300,
-    bypassToken: FETCH_BYPASS_KEY,
-  },
+	isr: {
+		expiration: 300,
+		bypassToken: FETCH_BYPASS_KEY
+	}
 };
 
 export const load = (async () => {
 	const pb: PocketBase = new PocketBase(`https://${domain}`);
-	const date = new Date().toJSON().slice(0, 10);
+
 	const result: ListResult<Times> = await pb.collection('times').getList<Times>(1, 3, {
-		filter: `from >= "${date} 00:00:00"`,
+		filter: `from >= "${moment(new Date()).tz('Europe/Vienna').format('YYYY-MM-DD')} 00:00:00"`,
 		sort: 'from'
 	});
 	const p: Times[] = structuredClone(result.items);
