6f006547c306932d7afd68a8b250e7a9152c642c
LiamKrenn <k.liam1224@gmail.com>
2023-08-20 01:14:07 +0200

    test implementation of revalidation

---
+++ src/routes/api/revalidate.ts
@@ -0,0 +1,21 @@
+import { FETCH_BYPASS_KEY } from '$env/static/private';
+
+type Headers = Record<string, string>;
+
+export async function get({ headers }: { headers: Headers}) {
+  // Check for the bypassToken in the x-prerender-revalidate header
+  if (headers['x-prerender-revalidate'] === FETCH_BYPASS_KEY) {
+    fetch('https://3canaries.com', {
+      headers: {
+        'x-prerender-revalidate': FETCH_BYPASS_KEY
+      }
+    })
+  }
+
+  // Return a response
+  return {
+    body: {
+      message: 'Revalidation triggered'
+    }
+  };
+}
