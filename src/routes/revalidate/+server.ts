a0f5a1425c002d99d013111b18061e3bd8e2a269
LiamKrenn <k.liam1224@gmail.com>
2023-08-24 00:42:48 +0200

    updated cors config

--- src/routes/revalidate/+server.ts
+++ src/routes/revalidate/+server.ts
@@ -15,9 +15,9 @@ async function revalidateRoute(route: string) {
 }
 
 const corsHeaders = { "Access-Control-Allow-Credentials": "true",
-"Access-Control-Allow-Origin": "https://admin.3canaries.com",
+"Access-Control-Allow-Origin": "*",
 "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-"Access-Control-Allow-Headers": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, x-prerender-revalidate" }
+"Access-Control-Allow-Headers": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
 
 export const OPTIONS: RequestHandler = async () => {
 	return new Response('ok', { headers: corsHeaders });
