e11fd9f6116ddc2587add847f9ea81d1c466d8fa
LiamKrenn <k.liam1224@gmail.com>
2024-09-06 22:14:25 +0200

    analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,7 @@
 <script lang="ts">
 	import '../theme.postcss';
-	// This contains the bulk of Skeletons required styles:
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
-	// Finally, your application's global stylesheet (sometimes labeled 'app.css')
 	import '../app.postcss';
-
-	import { dev } from '$app/environment';
-	import { inject } from '@vercel/analytics';
-
-	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <slot />
