6df0eb55cb080f794e1307528c8998c96b7e5e0d
Liam Krenn <k.liam1224@gmail.com>
2025-04-25 17:08:27 +0200

    potential fix

--- src/routes/revalidate/+server.ts
+++ src/routes/revalidate/+server.ts
@@ -2,7 +2,6 @@ import { FETCH_BYPASS_KEY } from '$env/static/private';
 import type { RequestHandler } from '@sveltejs/kit';
 import type { Handle } from '@sveltejs/kit';
 
-
 const routes: string[] = ['/', '/partner', '/open-times', '/news'];
 
 async function revalidateRoute(route: string) {
@@ -14,10 +13,13 @@ async function revalidateRoute(route: string) {
 	});
 }
 
-const corsHeaders = { "Access-Control-Allow-Credentials": "true",
-"Access-Control-Allow-Origin": "*",
-"Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-"Access-Control-Allow-Headers": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, x-prerender-revalidate" }
+const corsHeaders = {
+	'Access-Control-Allow-Credentials': 'true',
+	'Access-Control-Allow-Origin': '*',
+	'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+	'Access-Control-Allow-Headers':
+		'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, x-prerender-revalidate'
+};
 
 export const OPTIONS: RequestHandler = async () => {
 	return new Response('ok');
@@ -25,9 +27,9 @@ export const OPTIONS: RequestHandler = async () => {
 
 export const GET: RequestHandler = async ({ request }) => {
 	if (request.headers.get('x-prerender-revalidate') === FETCH_BYPASS_KEY) {
-		routes.forEach(async (route) => {
+		for (const route of routes) {
 			await revalidateRoute(route);
-		});
+		}
 		return new Response('Success');
 	}
 	return new Response('Invalid');
